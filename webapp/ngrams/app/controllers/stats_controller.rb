3e911dbd036730076f5f4418b19e547cefb16062
Mathieu Berube <mathieu.berube@gmail.com>
2012-04-28 18:55:56 -0400

    fix

--- webapp/ngrams/app/controllers/stats_controller.rb
+++ webapp/ngrams/app/controllers/stats_controller.rb
@@ -12,17 +12,27 @@ class StatsController < ApplicationController
     data_ngram = []
     legend = ['wiki', 'ngram']
     stats.each do |stat|
-      data_wiki << stat.wiki_freq
-      data_ngram << stat.ngram_freq
+      if stat
+        data_wiki << stat.wiki_freq
+        data_ngram << stat.ngram_freq
+      else
+        data_wiki << 0
+        data_ngram << 0
+      end
     end
 
 
-    @chart = Gchart.bar(:title => 'graph',
+    if terms.count <= 0
+    else
+      @chart = Gchart.bar(:title => 'graph',
                         :data => [data_wiki, data_ngram],
+                        :width => '600',
+                        :height => '400',
                         :legend => legend,
                         :stacked => false,
                         #:orientation => 'horizontal',
                         :hAxes => {:title => 'blah'},
                         :bar_colors => 'FF0000,00FF00')
+    end
   end
 end
