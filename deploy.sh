4885c4168cade1d1868abae90ea5d2ccc6706b6c
Che <che@che-Inspiron-1525.(none)>
2012-04-28 18:25:29 -0400

    Fixed the scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-scp -i /Users/mberube/.ssh/hackreduce.pem build/libs/Hackathon-0.2.jar hadoop@cluster-7.hackreduce.org:~/users/awesome
+scp -i ~/.ssh/hackreduce.pem build/libs/Hackathon-0.2.jar hadoop@cluster-7.hackreduce.org:~/users/awesome
